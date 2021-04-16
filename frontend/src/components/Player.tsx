4fac91d7b6e297964f061a0fcb3548a9c36ad388
Priscilla Camargo <priscillacamargo@Priscillas-MacBook-Pro.local>
2021-04-16 15:42:26 +0200

    Get user

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,35 +1,33 @@
-import * as React from "react";
-import Countdown from "components/Countdown";
+import * as React from 'react';
+import Countdown from 'components/Countdown';
 import { TPlayerProps } from '@types';
 import useGameState from '../hooks/gameState';
 
-
-import styles from '~/styles/player.module.scss'
+import styles from '~/styles/player.module.scss';
 
 export default function Player({ id, timeLeft }: TPlayerProps) {
-    const gameState = useGameState()
+  const gameState = useGameState();
 
-    return (
-        <section className={styles.wrapper}>
-            {
-                id ? (
-                    <>
-                        <header>
-                            <h2>{gameState?.getPlayerById(id)?.name}</h2>
-                        </header>
-                        <Countdown
-                            seconds={300}
-                            size={60}
-                            strokeBgColor="transparent"
-                            strokeColor="rgba(255, 255, 255, .7)"
-                            strokeWidth={8}
-                            countdown={timeLeft}
-                            isPlaying={true}
-                        />
-                    </>
-                ) : <>Pending for user</>
-
-            }
-        </section>
-    )
+  return (
+    <section className={styles.wrapper}>
+      {id ? (
+        <>
+          <header>
+            <h2>{gameState?.getPlayerById(id)?.name}</h2>
+          </header>
+          <Countdown
+            seconds={300}
+            size={60}
+            strokeBgColor="transparent"
+            strokeColor="rgba(255, 255, 255, .7)"
+            strokeWidth={8}
+            countdown={timeLeft}
+            isPlaying={true}
+          />
+        </>
+      ) : (
+        <>Pending for user</>
+      )}
+    </section>
+  );
 }
