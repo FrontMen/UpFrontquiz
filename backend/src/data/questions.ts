63df30a16fdf0a6f31dcb230454741924466b589
Randynamic <randy@randynamic.com>
2021-04-16 15:32:51 +0200

    Added questions

--- backend/src/data/questions.ts
+++ backend/src/data/questions.ts
@@ -8,13 +8,57 @@ export interface QuestionWithAnswers {
 export const questions: QuestionWithAnswers[] = [
   {
     question:
-      "What is the first word you think of when you hear the word JavaScript",
+        "What is the first word you think of when you hear the word JavaScript",
     answers: [
-      { answer: "Typescript" },
-      { answer: "EcmaScript" },
-      { answer: "ES6" },
-      { answer: "Bla" },
-      { answer: "Yes" },
+      "Typescript" ,
+      "EcmaScript" ,
+      "React" ,
+      "VueJs" ,
+      "developer" ,
+    ],
+  },
+  {
+    question:
+        "UpFront devlopers are the best in...",
+    answers: [
+      "Javascript" ,
+      "React" ,
+      "NodeJS" ,
+      "VueJs" ,
+      "eating food" ,
+    ],
+  },
+  {
+    question:
+        "The cities we visited during our conference trips are...",
+    answers: [
+      "Singapore" ,
+      "London" ,
+      "Las Vegas" ,
+      "Utrecht" ,
+      "Paris" ,
+    ],
+  },
+  {
+    question:
+        "What is the most favourite food of the UpFronters",
+    answers: [
+      "Thai" ,
+      "Greek" ,
+      "Singapore" ,
+      "Turkish" ,
+      "Fast food" ,
+    ],
+  },
+  {
+    question:
+        "What are the top clients within Upfront",
+    answers: [
+      "Rabobank" ,
+      "NPO" ,
+      "Fontem" ,
+      "Jumbo" ,
+      "Quin" ,
     ],
   },
 ];
