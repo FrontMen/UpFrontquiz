34cf596591f1266ad2b792103463b882aeb0674e
Priscilla Camargo <priscillacamargo@Priscillas-MacBook-Pro.local>
2021-04-16 13:19:44 +0200

    Add apollo server provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '~/styles/globals.css'
+import '~/styles/globals.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client/react';
+
+const client = new ApolloClient({ uri: 'http://95.211.247.249:3000/graphql', cache: new InMemoryCache() });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
