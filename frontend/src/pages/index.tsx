92c4b1f65bda5a06a4130acf4807dc716be93eeb
Randynamic <randy@randynamic.com>
2021-04-16 10:32:17 +0200

    added initial html setup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,28 @@ import styles from '~/styles/index.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      GOGOGOOG
-    </div>
+    <main className={styles.container}>
+        <section >
+            <header>
+                <h2>Team 1</h2>
+            </header>
+        </section>
+        <section >
+            <header>
+                <h2>Answers</h2>
+            </header>
+        </section>
+        <section >
+            <header>
+                <h2>Team 2</h2>
+            </header>
+        </section>
+        <section >
+            <button>Pass</button>
+            <h1>Question?</h1>
+            <input type="text"/>
+            <button>Submit</button>
+        </section>
+    </main>
   )
 }
