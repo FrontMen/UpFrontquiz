808672a32df7f65abee646394ad4d1e2e6e93e38
Stefan Lemmen <stefan@lemmen.nl>
2021-04-16 11:22:46 +0200

    feat(questions): add questions component

---
+++ frontend/src/components/Questions.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from 'react';
+
+import styles from './Questions.module.scss';
+
+export interface QuestionsProps {
+  submitButtonText: string;
+}
+
+const Questions = ({ submitButtonText }: PropsWithChildren<QuestionsProps>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // event.preventDefault();
+    console.log(event);
+  };
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.questionBlock}>
+        <h1 className={styles.question}>Question</h1>
+      </div>
+      <form className={styles.answerForm} onSubmit={(event) => handleSubmit(event)} method="POST">
+        <input className={`${styles.answerField} ${styles.answerInput}`} autoComplete="off" name="answer" type="text" />
+        <input className={`${styles.answerField} ${styles.answerSubmit}`} type="submit" value={submitButtonText} />
+      </form>
+    </section>
+  );
+};
+
+export default Questions;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,29 +1,28 @@
-import styles from '~/styles/index.module.scss'
+import styles from '~/styles/index.module.scss';
+import Questions from 'components/Questions';
 
 export default function Home() {
   return (
     <main className={styles.container}>
-        <section >
-            <header>
-                <h2>Team 1</h2>
-            </header>
-        </section>
-        <section >
-            <header>
-                <h2>Answers</h2>
-            </header>
-        </section>
-        <section >
-            <header>
-                <h2>Team 2</h2>
-            </header>
-        </section>
-        <section >
-            <button>Pass</button>
-            <h1>Question?</h1>
-            <input type="text"/>
-            <button>Submit</button>
-        </section>
+      <section>
+        <header>
+          <h2>Team 1</h2>
+        </header>
+      </section>
+      <section>
+        <header>
+          <h2>Answers</h2>
+        </header>
+      </section>
+      <section>
+        <header>
+          <h2>Team 2</h2>
+        </header>
+      </section>
+      <section>
+        <button>Pass</button>
+        <Questions submitButtonText="Answer" />
+      </section>
     </main>
-  )
+  );
 }
