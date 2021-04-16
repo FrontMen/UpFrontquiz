0628b2a38d913d63cee1b7b2695e11da88256901
Priscilla Camargo <priscillacamargo@Priscillas-MacBook-Pro.local>
2021-04-16 11:31:36 +0200

    Add get timer method

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,4 +5,41 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getTimeDuration(): object {
+    const maxTime = 180000;
+    let startTime = null;
+    let stopTime = null;
+    let running = false;
+
+    function getStartTime(): void {
+      const startClock = new Date();
+      startTime = startClock.getTime();
+    }
+
+    function getStopTime(): void {
+      const stopClock = new Date();
+      stopTime = stopClock.getTime();
+    }
+
+    function timeDuration(): number {
+      getStartTime();
+      getStopTime();
+      const time = this.stopTime - this.startTime;
+
+      return time;
+    }
+
+    if (stopTime < maxTime) {
+      return {
+        message: `Answerd in: ${timeDuration()} s`,
+      };
+    }
+
+    if (stopTime >= maxTime) {
+      return {
+        message: 'Sorry your time is up',
+      };
+    }
+  }
 }
